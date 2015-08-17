c8e64bde488f673600f947098bdb8f2759fa5bdb
Pranav Ram <pranav.ram7@gmail.com>
2015-08-16 22:22:12 -0400

    add entity viewer

--- public/main.client.controller.js
+++ public/main.client.controller.js
@@ -4,17 +4,23 @@ angular.module('vizit')
 function MainCtrl($scope, $http) {
 	$scope.documents = [];
   $scope.entities = [];
-  $scope.selectedDocumentText = "";
+  $scope.selectedDocument = {};
   $scope.entityType = "Person";
   $scope.entityTypes = ["Person", "Organization", "City", "Quantity", "JobTitle", "FieldTerminology"];
   $scope.selectDocument = function(doc) {
-    $scope.selectedDocumentText = doc.text;
+    if($scope.selectedDocument._id !== doc._id){
+      doc.viewCount = doc.viewCount + 1;
+    };
+    $scope.selectedDocument = doc;
   }
   
   $http.get('/api/documents').
     then(function(response) {
-      $scope.documents = response.data;
-      $scope.selectedDocumentText = $scope.documents[0].text;
+      $scope.documents = response.data.map(function(data) {
+        data.viewCount = 0;
+        return data;
+      });
+      $scope.selectedDocument = $scope.documents[0];
     }, function(response) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
@@ -22,7 +28,10 @@ function MainCtrl($scope, $http) {
 
   $http.get('/api/entities').
     then(function(response) {
-      $scope.entities = response.data;
+      $scope.entities = response.data.map(function(data) {
+        data.count = Math.floor(Math.random() * 20) + 1;
+        return data;
+      });
     }, function(response) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
