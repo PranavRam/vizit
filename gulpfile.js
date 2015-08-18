6ac948d00a4ade71e088c00fe4071c4f8bbd61dc
Pranav Ram <pranav.ram7@gmail.com>
2015-08-18 19:05:38 -0400

    add initial hypothesis view

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var gulp = require('gulp'),
   bowerFiles = require('main-bower-files'),
   inject = require('gulp-inject'),
   es = require('event-stream')
-  watch = require('gulp-watch');
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create(),
+  reload = browserSync.reload;
 
 gulp.task('bower-inject', function() {
   gulp.src('./server/views/includes/*.html')
@@ -16,15 +18,47 @@ gulp.task('bower-inject', function() {
     .pipe(gulp.dest('./server/views/includes'));
 });
 
-gulp.task('develop', ['bower-inject'], function () {
-  nodemon({ script: 'index.js'
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init({
+      proxy: "localhost:8000",  // local node app address
+      port: 5000,  // use *different* port than above
+      notify: true,
+      files: ["./public/**/*", "!./public/lib"],
+      browser: "firefox"
+  });
+})
+
+gulp.task('nodemon', ['bower-inject'], function (cb) {
+  var started = false;
+  return nodemon({ script: 'index.js'
           , ext: 'html js'
           , watch: ['index.js', 'server/**/*']})
+    .on('start', function () {
+        // to avoid nodemon being started multiple times
+        // thanks @matthisk
+        console.log('nodemon started');
+        if (!started) {
+          setTimeout(function() {
+            browserSync.init({
+                proxy: "localhost:8000",  // local node app address
+                port: 5000,  // use *different* port than above
+                notify: true,
+                files: ["public/**/*", "!public/lib"],
+                browser: "firefox"
+            });
+          }, 1000);
+        }
+        started = true;
+      })
     .on('restart', function () {
-      console.log('restarted!')
-    })
+      console.log('restarted');
+      setTimeout(function () {
+            reload({ stream: false });
+          }, 3000);
+    });
 });
 
-gulp.task('default', ['develop'], function() {
+gulp.task('default', ['browser-sync'], function() {
    gulp.watch('./bower.json', ['bower-inject']);
 });
---
+++ public/d3hypothesis.js
@@ -0,0 +1,117 @@
+(function(d3) {
+	d3.vizit = {};
+	function Hypothesis() {
+		var opts = {
+			width: 250
+		}
+
+		var draggable = false;
+		var drag = d3.behavior.drag()
+		    .origin(function(d) { return d; })
+		    .on("dragstart", function() { 
+		    	d3.event.sourceEvent.stopPropagation();
+		    	var targetNode = d3.event.sourceEvent.target;
+					if(d3.select(targetNode).classed("header") || 
+						$(targetNode).parents('.header').length) {
+						draggable = true;
+					};
+		    })
+		    .on("drag", dragmove)
+		    .on("dragend", function(d) {
+		    	draggable = false;
+		    })
+
+		function dragmove(d) {
+			if(!draggable) return;
+			d.x += d3.event.dx;
+      d.y += d3.event.dy;
+		  d3.select(this)
+		      .attr({
+		      	x: d.x,
+		      	y: d.y
+		      });
+		}
+
+		function component(selection) {
+			selection.each(function(data, i) {
+				// debugger;
+				var parent = d3.select(this);
+				var fo = parent.append("foreignObject")
+    					    .attr({
+    					    	x:data.x,
+    					    	y:data.y
+    					    })
+				var wrapper = fo.append("xhtml:body")
+
+				var container = wrapper
+													.append("xhtml:div")
+													.attr("class", "hypothesis");
+
+				var header = container.append("xhtml:div").attr("class", "header");
+				var body = container.append("xhtml:div").attr("class", "body");
+
+				var title = header.append("xhtml:p").attr("class", "title");
+				var count = header.append("xhtml:div").attr("class", "count");
+				var showHide = header.append("xhtml:div").attr("class", "show-hide");
+
+				container.style({
+					"color": "white"
+				})
+				header
+					.style({
+						width: opts.width+"px",
+						height: 50+"px",
+						"background-color": "#263238",
+						padding: "8px"
+					})
+					.attr({
+						layout: "row",
+						"layout-align": "space-around center"
+					})
+
+
+				title.text("Hypothesis "+i)
+							.attr({
+								flex: ""
+							});
+
+				count.style({
+					"text-align": "center",
+					"background-color": "#4CAF50",
+					// "color": "white",
+					width: 25+"px",
+					height: 25+"px",
+					padding: "8px"
+				})
+				.text("5")
+				.attr({
+					// flex: "5"
+				})
+
+				showHide.style({
+					"text-align": "center",
+					padding: "8px"
+				})
+				.html('<i class="fa fa-chevron-down"></i>')
+				// .attr({
+				// 	flex: "5"
+				// })
+
+				body.style({
+					width: opts.width+"px",
+					"min-height": 150+"px",
+				})
+
+				var node = $(container.node());
+				fo.attr({
+					width: opts.width,
+					height: node.height()
+				});
+
+				fo.call(drag);
+			})
+		}
+		return component;
+	}
+	d3.vizit.hypothesis = Hypothesis;
+})(d3);
