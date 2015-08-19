ce9f9e664215b3edcaccf26bf404007f5de98aa4
Pranav Ram <pranav.ram7@gmail.com>
2015-08-19 00:45:18 -0400

    add intitial minimap

--- public/ach_canvas.client.directive.js
+++ public/ach_canvas.client.directive.js
@@ -7,32 +7,37 @@ function achCanvas($timeout) {
     link: function(scope, el, attrs) {
     	var hypothesis = d3.vizit.hypothesis();
     	var evidence = d3.vizit.evidence();
+    	var minimap = d3.minimap();
+    	var minimapScale = 0.05;
     	var borderScale = d3.scale.linear()
     														.domain([0.1, 1])
-    														.range([10, 3]);
+    														.range([11, 3]);
 
     	var compile = function() {
     		var achCanvas = $(el[0]);
     		var svg = d3.select(achCanvas.get()[0]).append("svg");
+    		var translation = [0, 0];
+    		var scale = 1;
 
     		svg.attr({
     			width: achCanvas.width(),
     			height: achCanvas.height()
     		});
 
-    		var container = svg.append("g");
+    		var container = svg.append("g")
+    												.attr({
+    													"class": "ach-canvas-container",
+    													"height": achCanvas.height(),
+    													"width": achCanvas.width()
+    												})
     		var hypotheses = container.selectAll('.hypothesis');
     		var evidences = container.selectAll('.evidence');
-    		var minimap = svg.append("rect")
-											    .attr("width", 250)
-											    .attr("height", 150)
-											    .style("fill", "gray")
-											    .attr({
-											    	x: achCanvas.width() - 8 - 250,
-											    	y: achCanvas.height() - 8 - 150
-											    })
-											    .style("pointer-events", "all");
 
+    		var minimapLoc = [
+    			achCanvas.width() - 8 - 250,
+    			achCanvas.height() - 8 - 150
+    		]
+    		
 				// var hypotheses = container.selectAll('.hypothesis');
 
 				// 										hypotheses
@@ -42,8 +47,8 @@ function achCanvas($timeout) {
 				// 											.call(hypothesis);
 
     		
-    		hypotheses = hypotheses.data([{x: 100, y: 100}, {x: 200, y: 200}]);
-    		evidences = evidences.data([{x: 300, y: 300}, {x: 400, y: 400}]);
+    		hypotheses = hypotheses.data([{x: 100, y: 100}]);
+    		evidences = evidences.data([{x: 300, y: 300}]);
 
     		var fb = hypotheses
     							.enter()
@@ -53,25 +58,40 @@ function achCanvas($timeout) {
 	  		var fb = evidences
 	  							.enter()
 	  							.append('g')
-						      .call(evidence);
-
-		    // var div = fb.enter()
-								  	
+						      .call(evidence);								  	
 
 			  var zoom = d3.behavior.zoom()
 			      .scaleExtent([0.1, 1])
-			      .on("zoom", zoomed);
+			      .on("zoom", zoomHandler);
 
-				function zoomed() {
-				  container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-					var scale = d3.event.scale;
+				function zoomHandler(newScale) {
+					newScale ? scale = newScale : scale = d3.event.scale;
+					translation = d3.event ? d3.event.translate : [0, 0];
+				  container.attr("transform", "translate(" + translation + ")scale(" + scale + ")");
+
 					container.selectAll(".body")
 										.style({
 											"border-width": Math.round(borderScale(scale))+"px"
 										});
+
+					minimap.scale(scale).render();
 				}
 				// fb.call(drag);
 				svg.call(zoom);
+
+				minimap
+		    		.zoom(zoom)
+		    		.target(container)
+		    		.minimapScale(minimapScale)
+		    		.minimapWidth(250)
+		    		.minimapHeight(150)
+		    		.x(minimapLoc[0])
+		    		.y(minimapLoc[1]);
+
+		    svg.call(minimap);
+
+		    zoom.scale(scale);
+		    zoomHandler(scale);
 
 				scope.$watch('ach.fullscreen', function(newVal, oldVal) {
 					if(newVal !== oldVal) {
@@ -83,12 +103,12 @@ function achCanvas($timeout) {
 									height: achCanvas.height()
 								});
 
-							minimap
-								// .transition()
-								.attr({
-						    	x: achCanvas.width() - 8 - 250,
-						    	y: achCanvas.height() - 8 - 150
-						    })
+							// minimap
+							// 	// .transition()
+							// 	.attr({
+						 //    	x: achCanvas.width() - 8 - 250,
+						 //    	y: achCanvas.height() - 8 - 150
+						 //    })
 						})
 					}
 				})
---
+++ public/minimap.js
@@ -0,0 +1,171 @@
+(function(d3) {
+	d3.minimap = d3.minimap || {};
+	
+	function getXYFromTranslate(translateString) {
+		var split = translateString.split(",");
+		var x = split[0] ? ~~split[0].split("(")[1] : 0;
+		var y = split[1] ? ~~split[1].split(")")[0] : 0;
+		return [x, y];
+	}
+	function Minimap() {
+		var opts = {
+			x: 0,
+			y: 0,
+			canvasWidth: 0,
+			canvasHeight: 0,
+			minimapWidth: 0,
+			minimapHeight: 0,
+			minimapBgColor: "#e0e0e0",
+			minimapHandleColor: "#757575",
+			minimapHandleX: 0,
+			minimapHandleY: 0,
+			zoom: null,
+			target: null,
+			scale: 1,
+			minimapScale: 0.15
+		}
+
+		function component(selection) {
+			var svg = selection;
+
+			var minimapContainer = svg.append("g")
+																.attr("class", "minimap-container");
+
+			var minimapBg =	minimapContainer
+												.append("rect")
+												.attr("class", "minimap-background");
+
+			var minimap = minimapContainer
+												.append("g")
+												.attr("class", "minimap-content");
+
+			var minimapHandleContainer = minimap.append("g")
+																		.attr("class", "minimap-handle-container");
+			var minimapClone = minimap.append("g").attr("class", "minimap-clone");
+
+			var minimapHandle = minimapHandleContainer
+																			.append("rect")
+																			.attr({
+																				"class": "minimap-handle",
+																				"width": opts.canvasWidth,
+																				"height": opts.canvasHeight,
+																			});
+
+			function dragstart() {
+				var frameTranslate = getXYFromTranslate(minimapHandleContainer.attr("transform"));
+				opts.minimapHandleX = frameTranslate[0];
+				opts.minimapHandleY = frameTranslate[1];
+			}
+
+			function dragmove() {
+				d3.event.sourceEvent.stopImmediatePropagation();
+				opts.minimapHandleX += d3.event.dx;
+				opts.minimapHandleY += d3.event.dy;
+
+				var minimapHandleTransform = [
+					opts.minimapHandleX,
+					opts.minimapHandleY
+				];
+
+				minimapHandleContainer
+						.attr({
+							"transform": "translate("+minimapHandleTransform+")"+"scale("+ opts.minimapScale +")"
+						});
+
+				var translate = [
+					-opts.minimapHandleX * 1 / opts.minimapScale * opts.scale,
+					-opts.minimapHandleY * 1 / opts.minimapScale * opts.scale,
+				];
+				// console.log(opts.scale);
+				opts.target
+								.attr({
+									"transform": "translate(" + translate + ")scale(" + opts.scale + ")"
+								})
+				opts.zoom.translate(translate);
+				// opts.zoom.event(opts.target);
+			}
+			var drag = d3.behavior.drag()
+									.on("dragstart.minimap", dragstart)
+									.on("drag.minimap", dragmove);
+
+			opts.zoom.on("zoom.minimap", function() {
+				opts.scale = d3.event.scale;
+			});
+
+			minimapHandleContainer.call(drag);
+
+			component.render = function() {
+				opts.scale = opts.zoom.scale();
+
+				minimapContainer
+					.attr({
+						"transform": "translate("+ [opts.x, opts.y] +")"
+					});
+
+				minimapClone
+					.attr({
+						"transform": "scale("+ opts.minimapScale +")"
+					});
+
+				minimapBg
+						.attr({
+							"width": opts.minimapWidth,
+							"height": opts.minimapHeight
+						})
+						.style({
+							"fill": opts.minimapBgColor
+						})
+
+				var targetTranslate = getXYFromTranslate(opts.target.attr("transform"));
+				var minimapHandleTransform = [
+					-targetTranslate[0] / opts.scale,
+					-targetTranslate[1] / opts.scale
+				];
+
+				minimapHandleContainer
+						.attr({
+							"transform": "translate("+ minimapHandleTransform +")" +"scale("+ opts.minimapScale +")"
+						});
+
+				minimapHandle
+								.attr({
+									"width": opts.canvasWidth / opts.scale,
+									"height": opts.canvasHeight / opts.scale,
+								})
+								.style({
+									fill: opts.minimapHandleColor
+								})
+				var clone = opts.target.node().cloneNode(true);
+				clone.removeAttribute("transform");
+				minimapClone.selectAll('.ach-canvas-container').remove();
+				minimapClone.node().appendChild(clone);
+
+			}
+		}
+
+		function accessor(key) {
+			return function(value) {
+				if(!arguments.length) return opts[key];
+				opts[key] = value;
+				return component;
+			}
+		}
+
+		for(var n in opts) {
+			if(opts.hasOwnProperty(n)) {
+				component[n] = accessor(n);
+			}
+		}
+
+		component.target = function(value) {
+			if(!arguments.length) return opts.target;
+			opts.target = value;
+			opts.canvasWidth = +value.attr("width");
+			opts.canvasHeight = +value.attr("height");
+			return component;
+		}
+
+		return component;
+	}
+	d3.minimap = Minimap;
+})(d3);
